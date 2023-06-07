e81c285e5e1ebbb73c1d821e37460add453a6dd8
warrenhong <warren@airkit.com>
2023-06-07 09:45:38 -0700

    send event

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-
   useEffect(() => {
-    window.addEventListener("message", (event) => {console.log(event)})
-  },[])
+    window.addEventListener("message", (event) => {
+      console.log(event);
+    });
+
+  }, []);
+
+  const URL = "https://app.ruist.com/l/goEx"
+
+  const onClick = () => {
+    const payload = {
+      eventSourceKey: "incoming",
+      data: {
+        var: "hello"
+      }
+    }
+    window.postMessage(payload,URL)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-      </header>
+      <header className="App-header">hello</header>
+      <button onClick={onClick}>send data to airkit</button>
     </div>
   );
 }
